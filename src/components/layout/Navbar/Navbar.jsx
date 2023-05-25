5fcd0213512cd293bde46f806b97999e88c319f4
julian corbo <corbojulian@gmail.com>
2023-05-25 10:35:12 -0300

    git graph

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
+import { Button } from "@mui/material"
+import "./Navbar.css"
+import {AiOutlineShoppingCart} from "react-icons/ai"
+
 const Navbar = () =>{
 
     return(
-        <div>
-            <h4>imagen</h4>
-            <ul>
-                <li>Todos los productos</li>
-                <li>Accesorios</li>
-                <li>Repuestos</li>
+        <div className="contenedor-navbar">
+            <img src="" alt="" />
+            <ul className="menu">
+            <Button href="/" variant="contained">
+                    Shop
+            </Button>
+            <Button href="/" variant="contained">
+                     Ofertas
+            </Button>
+            <Button href="/" variant="contained">
+                    Nuevos
+            </Button>
             </ul>
+
+            {/* react icons */}
+            <AiOutlineShoppingCart size={70}/>
         </div>
     )
 
